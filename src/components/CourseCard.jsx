d3126176d4c736aabecf5f7b55bfda55db9e0e4f
Abdul Hameed <sd.hameed79@gmail.com>
2024-08-27 12:32:51 +0530

    Initial commit

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -17,24 +17,24 @@ const CourseCard = ({course}) => {
     }
 
     return (
-        <div className="bg-slate-800 rounded-lg p-2 shadow-md transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-black">
-            <div>
-                <div className="relative">
-                    <img src={course.image.url} alt={course.title} className="rounded-md object-cover"/>
-                    <button 
-                        className="absolute w-7 h-7 bg-white rounded-full right-2 top-32 grid place-items-center flex-wrap"
-                        onClick={HandlerToast}>
-                            {liked.includes(course.id) ?  <FcLike fontSize="1.3rem"/> : <FcLikePlaceholder fontSize="1.3rem"/> }
-                    </button>
+            <div className="bg-slate-800 rounded-lg p-2 shadow-md transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-black flex-wrap">
+                <div>
+                    <div className="relative">
+                        <img src={course.image.url} alt={course.title} className="rounded-md object-cover"/>
+                        <button 
+                            className="absolute w-7 h-7 bg-white rounded-full right-1 top-32 grid place-items-center flex-wrap"
+                            onClick={HandlerToast}>
+                                {liked.includes(course.id) ?  <FcLike fontSize="1.3rem"/> : <FcLikePlaceholder fontSize="1.3rem"/> }
+                        </button>
+                    </div>
+                    <h1 className="text-white font-medium text-lg mb-2"> {course.title}  </h1>
+                    <p className="text-white "> 
+                        {course.description.length > 100 ? 
+                        course.description.substr(0, 100) + "..." :
+                        course.description}
+                    </p>
                 </div>
-                <h1 className="text-white font-medium text-lg mb-2"> {course.title}  </h1>
-                <p className="text-white "> 
-                    {course.description.length > 100 ? 
-                     course.description.substr(0, 100) + "..." :
-                     course.description}
-                </p>
             </div>
-        </div>
     )
 }
 
